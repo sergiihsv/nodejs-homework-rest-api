9b738d79910c0a98cc37b87636d0e395e951cb23
sergiihsv <hsv-serg@ukr.net>
2022-10-13 14:03:20 +0300

    add path

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,14 @@
-// const fs = require('fs/promises')
+/* const fs = require("fs/promises");
 
-const listContacts = async () => {}
+const listContacts = async () => {};
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {};
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {};
 
-const addContact = async (body) => {}
+const addContact = async (body) => {};
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {};
 
 module.exports = {
   listContacts,
@@ -16,4 +16,52 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
+}; */
+
+const fs = require("fs").promises;
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
 }
+
+async function getContactById(id) {
+  const contacts = await listContacts();
+  const contactById = contacts.find((contact) => contact.id === id);
+  return contactById || null;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name: name,
+    email: email,
+    phone: phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+}
+
+async function removeContact(id) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === id);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+};
