20c45fc71ce48d11157d3884189ad55062d848e5
sergiihsv <hsv-serg@ukr.net>
2022-10-13 22:00:05 +0300

    add helpers

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,20 +1,54 @@
 const express = require("express");
 
 const contacts = require("../../models/contacts");
+/* const Joi = require("joi"); */
+
+const { RequestError } = require("../../helpers");
 
 const router = express.Router();
 
+/* const addShema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+}); */
+
 router.get("/", async (req, res, next) => {
-  const result = await contacts.listContacts();
-  res.json(result);
+  try {
+    const result = await contacts.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.getContactById(contactId);
+    if (!result) {
+      throw RequestError(404, "Not found");
+
+      /*  const error = new Error("Not found");
+      error.status = 404;
+      throw error; */
+      /*  return res.status(404).json({
+        message: "Not found",
+      }); */
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const result = await contacts.addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.delete("/:contactId", async (req, res, next) => {
