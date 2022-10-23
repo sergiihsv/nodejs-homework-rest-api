106d81e2d19f64302acc3b33de6cf8a85b5a02d6
sergiihsv <hsv-serg@ukr.net>
2022-10-24 02:51:34 +0300

    add dotenv

--- app.js
+++ app.js
@@ -3,12 +3,22 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+dotenv.config();
+
+const { DB_HOST } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connection successful"))
+  .catch((error) => console.log(error.message));
 
 app.use(logger(formatsLogger));
 app.use(cors());
