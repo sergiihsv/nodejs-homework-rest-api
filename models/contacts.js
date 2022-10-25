cd29f9d6d772cee8ba8f24158ad0f719befa935d
sergiihsv <hsv-serg@ukr.net>
2022-10-26 02:30:17 +0300

    add schemas

--- models/contacts.js
+++ models/contacts.js
@@ -1,4 +1,4 @@
-const fs = require("fs").promises;
+/* const fs = require("fs").promises;
 const path = require("path");
 const { nanoid } = require("nanoid");
 
@@ -57,3 +57,4 @@ module.exports = {
   updateContact,
   removeContact,
 };
+ */
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,25 +3,25 @@ const express = require("express");
 const ctrl = require("../../controllers/contacts");
 const { ctlrWrapper } = require("../../helpers");
 const { validateBody } = require("../../middlewares");
-const schemas = require("../../schemas/contacts");
+const { schemas } = require("../../models/contact");
 const router = express.Router();
 
-router.get("/", ctlrWrapper(ctrl.getAllListContacts));
+/* router.get("/", ctlrWrapper(ctrl.getAllListContacts));
 
-router.get("/:id", ctlrWrapper(ctrl.getById));
+router.get("/:id", ctlrWrapper(ctrl.getById)); */
 
 router.post(
   "/",
-  validateBody(schemas.addShema),
+  validateBody(schemas.addSchema),
   ctlrWrapper(ctrl.addNewContacts)
 );
 
-router.put(
+/* router.put(
   "/:id",
   validateBody(schemas.addShema),
   ctlrWrapper(ctrl.updateById)
 );
 
-router.delete("/:id", ctlrWrapper(ctrl.deleteById));
+router.delete("/:id", ctlrWrapper(ctrl.deleteById)); */
 
 module.exports = router;
