7206538a8fbdd4e2517d9819e935a591ac4cfaef
sergiihsv <hsv-serg@ukr.net>
2022-11-06 04:25:08 +0200

    add jwt

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,3 +1,10 @@
-const authenticate = async (req, res, next) => {};
+const { RequestError } = require("../helpers");
+
+const authenticate = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = authenticate;
