f45f24439269cba730b729b6fd8720334ac32130
sergiihsv <hsv-serg@ukr.net>
2022-11-10 05:07:06 +0200

    add Jimp

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,6 +1,7 @@
 const { User } = require("../../models/user");
 const fs = require("fs/promises");
 const path = require("path");
+const Jimp = require("jimp");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
@@ -12,7 +13,22 @@ const updateAvatar = async (req, res) => {
   const resultUpload = path.join(avatarsDir, filename);
   await fs.rename(tempUpload, resultUpload);
   const avatarURL = path.join("avatars", filename);
+  console.log(avatarURL);
   await User.findByIdAndUpdate(_id, { avatarURL });
+
+  /*  Jimp.read("filename", (err, filename) => {
+    if (err) throw err;
+    filename.resize(250, 250);
+  }); */
+
+  Jimp.read("lenna.png", (err, lenna) => {
+    if (err) throw err;
+    lenna
+      .resize(256, 256) // resize
+      .quality(60) // set JPEG quality
+      .greyscale() // set greyscale
+      .write("lena-small-bw.jpg"); // save
+  });
 
   res.json({
     avatarURL: avatarURL,
