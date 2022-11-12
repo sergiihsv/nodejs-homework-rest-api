656570593fb63fda14bdfbdeddd32fff5355ebca
sergiihsv <hsv-serg@ukr.net>
2022-11-12 17:48:46 +0200

    add mail

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,7 +1,10 @@
 const { User } = require("../../models/user");
-const { RequestError } = require("../../helpers");
+const { RequestError, sendEmail } = require("../../helpers");
 const bcrypt = require("bcryptjs");
 const gravatar = require("gravatar");
+const { nanoid } = require("nanoid");
+
+const { BASE_URL } = process.env;
 
 const signup = async (req, res) => {
   const { email, password } = req.body;
@@ -11,11 +14,22 @@ const signup = async (req, res) => {
   }
   const hashPassword = await bcrypt.hash(password, 10);
   const avatarURL = gravatar.url(email);
+  const verificationToken = nanoid();
   const result = await User.create({
     email,
     password: hashPassword,
     avatarURL,
+    verificationToken,
   });
+
+  const mail = {
+    to: email,
+    subject: "Verification email",
+    html: `<a target= "_blank" href="${BASE_URL}/api/auth/users/verify/${verificationToken}">Click to verify you email</a>`,
+  };
+
+  await sendEmail(mail);
+
   res.status(201).json({
     email: result.email,
     subscription: "starter",
